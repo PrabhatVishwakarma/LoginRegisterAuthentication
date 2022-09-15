da05f2cae0ed2f437d7e193e3c5df03adb2aaa35
Prabhat Vishwakarma <83131972+PrabhatVishwakarma@users.noreply.github.com>
2022-09-15 16:04:18 +0530

    Add project files.

---
+++ LoginRegisterAuthentication/Controllers/AuthController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace LoginRegisterAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration Configuration)
+        {
+            _configuration = Configuration;
+                
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            CreatePasswordHash(request.Password, out byte[] PasswordHash, out byte[] PasswordSalt);
+            
+            user.UserName = request.UserName;
+            user.PasswordHash = PasswordHash;
+            user.PasswordSalt = PasswordSalt;
+            
+            return Ok(user);
+        }
+        [HttpPost("Login")]
+        public async Task<ActionResult<string>> Login(UserDto request)
+        {
+            if (user.UserName != request.UserName)
+            {
+                return BadRequest("User Not Found");
+            }
+            if(!verifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                return BadRequest("Wrong Password");
+            }
+            string token = CreateToken(user);
+            return Ok(token);
+        }
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds);
+            
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+
+            return jwt;
+        }
+
+        private void CreatePasswordHash(String Password, out byte[] PasswordHash, out byte[] PasswordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                PasswordSalt = hmac.Key;
+                PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(Password));
+            }
+        }
+
+        private bool verifyPasswordHash(string Password, byte[] Passwordhash, byte[] PasswordSalt)
+        {
+            using( var hmac = new HMACSHA512(PasswordSalt))
+            {
+                var computeHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(Password));
+                return computeHash.SequenceEqual(Passwordhash);
+            }
+        }
+    }
+}
